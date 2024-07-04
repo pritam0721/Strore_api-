49b178ec4b4eb5d9e073d6bf3d890d48d7ab3d40
pritam0721 <pritamgayen629@gmail.com>
2024-07-05 03:35:24 +0530

    final commit for store api

--- controllers/products.js
+++ controllers/products.js
@@ -1,12 +1,83 @@
 
- const getAllProductsStatic =  (req,res)=>{
-    //! for throwing error   throw new Error('the error from async products')
-       res.status(200).json({img:'product testing route'})
+const Product = require('../models/product')
+
+ const getAllProductsStatic =  async (req,res)=>{
+          const products = await Product.find({
+             featured:true,
+             name:'wooden table'
+          })
+       res.status(200).json({products,nbHits:products.length})
  }
 
- const getAllProducts =  (req,res)=>{
-       res.status(200).json({img:'product route'})
+ const getAllProducts = async (req,res)=>{
+        const {featured , company , name, sort, fields , numericFilters } = req.query
+       
+        const queryObject = {}
+
+        if(featured){
+             queryObject.featured = featured === 'true' ? true : false
+        }  
+        if(company){
+             queryObject.company = company
+             
+        }
+        if(name){
+             queryObject.name = {$regex:name,$options:'i'}
+             
+        }
+        
+        if(numericFilters){
+             const operatorMap = {
+                '>': '$gt',
+                '>=': '$gte',
+                '=': '$eq',
+                '<': '$lt',
+                '<=': '$lte',
+             };
+             const regEx = /\b(<|>|>=|=|<|<=)\b/g;
+
+             let filters = numericFilters.replace(
+                 regEx,(match)=>`-${operatorMap[match]}-`
+             );
+
+          const options = ['price','rating'];
+          filters = filters.split(',').forEach((item) => {
+            const [field,operator,value] = item.split('-');
+
+            if(options.includes(field))
+                {
+                     queryObject[field]={[operator]:Number(value)};
+                }
+          });
+            
+        }
+      
+  console.log(queryObject);
+    let result  =  Product.find(queryObject,{_id:0})
+
+     if(sort){
+           const  sortList = sort.split(',').join(' ')
+           result = result.sort(sortList)
+     }
+     else{
+         result = result.sort( 'createdAt')
+     }
+     if(fields){
+         const fieldsList  = fields.split(',').join(' ')
+          result = result.select(fieldsList)
+     }
+    const page = Number(req.query.page) || 1
+    const limit = Number(req.query.skip) || 10
+    const skip = (page - 1) * limit
+      
+     result = result.skip(skip).limit(limit)
+
+       const products = await result
+
+       res.status(200).json({products ,nbHits:products.length})
  }
+
+ 
 
  module.exports = {  getAllProductsStatic,
                     getAllProducts,
